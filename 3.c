66df9d8b0f355c9073755ad139da2be09802fdc0
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-04-27 23:00:17 +0530

    3.c added

---
+++ 3.c
@@ -0,0 +1,10 @@
+/*Size of various datatypes*/
+#include <stdio.h> 
+#define pr printf
+int main(){
+pr("Size of int is %d\n",sizeof(int));
+pr("Size of float is %d\n",sizeof(float));
+pr("Size of double is %d\n",sizeof(double));
+pr("Size of char is %d\n",sizeof(char));
+pr("Size of void is %d\n",sizeof(void));
+}
