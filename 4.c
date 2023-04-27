37053d8f02512038a6368c2cb05936c804596b7f
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-04-27 22:12:44 +0530

    4th prob added

---
+++ 4.c
@@ -0,0 +1,8 @@
+/* Sum and Average of 3 values */
+#include <stdio.h> 
+int main(){
+	float a,b,c;
+	printf("Enter three numbers:");
+	scanf("%f%f%f",&a,&b,&c);
+	printf("Sum : %f\nAverage : %f\n",a+b+c,(a+b+c)/3);
+}
