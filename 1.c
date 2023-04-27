dfa7e8c4768d11a4c7e03437f88b4cf1cd9ff6dd
eldhosekroy <eldhoseroy12@gmail.com>
2023-04-27 20:13:21 +0530

    oneans

--- 1.c
+++ 1.c
@@ -1,4 +1,14 @@
+//prgm by eldhose
+//To find b^2-4ac
+
+
 #include<stdio.h>
 int main(){
+
+int a,b,c,sum;
+printf("Enter 3 numbers : ");
+scanf("%d%d%d",&a,&b,&c);
+sum = (b*b)-4*a*c;
+printf("Answer is %d",sum);
 
 }
