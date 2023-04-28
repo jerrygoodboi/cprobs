b9fb10aea094a3fd4433efdc02f74d8a6e587012
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-04-28 22:01:07 +0530

    22_1_fact.c added

---
+++ 22_1_fact.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <stdint.h>
+uint64_t fact(uint64_t num){
+	if(num == 0){
+		return 1;
+	}
+	else{
+		for(int i=num-1;i>0;i--){
+		num*=i;
+	}
+		return num;
+	}
+}
+int main(){
+	int n,val;
+	double sum=0;
+	scanf("%d",&n);	
+	for(int i=0;i<n;i++){
+	 	sum+=((double)1/fact(i));
+	}
+	printf("Sum is :%.52f\n",sum);
+}
