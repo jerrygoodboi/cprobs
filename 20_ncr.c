04596cf86edb946c0bbe8b045844fed9589f5a33
jerry <rabbitron295@gmail.com>
2023-04-28 17:33:37 +0530

    20_ncr.c added

---
+++ 20_ncr.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+int fact(int num){
+	if(num==0){
+		return 1;
+	}
+	else{
+	for(int i=num-1;i>0;i--){
+	num*=i;
+	}
+	return num;
+	}
+}
+int main(){
+	int n,r;
+	printf("Enter n and r:");
+	scanf("%d%d",&n,&r);
+	printf("Ans is:%d\n",fact(n)/(fact(r)*fact(n-r)));
+}
