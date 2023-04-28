8c163f1c8a10c35dfd63490bbf23fd74552882ba
jerry <rabbitron295@gmail.com>
2023-04-28 16:54:27 +0530

    38_primerange.c added

---
+++ 38_primerange.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+int main(){
+	int num,flag;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	for(int j=2;j<num;j++){
+		flag=0;
+	for(int i=2;i<(j/2)+1;i++){
+		if(j%i==0){
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0){
+		printf("%d\n",j);
+	}
+	}
+}
