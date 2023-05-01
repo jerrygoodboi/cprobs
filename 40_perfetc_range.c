23aeccd791f77c509bfe860de46093f14fcbe8da
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-05-01 13:53:06 +0530

    40_perfetc_range.c added

---
+++ 40_perfetc_range.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#define pr printf
+int main(){
+	int low,high,sum;
+	pr("Enter the range, first lowest then highest:\n");
+	scanf("%d%d",&low,&high);
+	for(int i=low;i<=high;i++){
+		sum = 0;
+		for(int j=1;j<i;j++){
+			if(i%j == 0){
+			sum+=j;
+			}
+		}	
+		if(sum == i){
+		pr("%d\n",i);
+		}
+	}
+}
