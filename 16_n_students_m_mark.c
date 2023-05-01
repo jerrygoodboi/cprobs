52e54c8a7684a8bc3803724bb08b55e83c86dd2f
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-05-01 14:35:39 +0530

    16_n_students_m_mark.c added

---
+++ 16_n_students_m_mark.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#define pr printf
+#define sc scanf
+int main(){
+	int sum,mark,n,m;
+	pr("Enter the no. of students and  No. of subjects:");
+	scanf("%d%d",&n,&m);
+	for(int i=1;i<=n;i++){
+		sum =0;
+		mark=0;
+		pr("Student %d:\n",i);
+		for(int j=0;j<m;j++){
+			pr("Enter the marks:");
+			scanf("%d",&mark);
+			sum+=mark;
+		}
+		pr("Total mark for student %d is %d\n",i,sum);
+	}
+}
