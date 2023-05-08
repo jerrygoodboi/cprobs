9cd479cf5d79987b0534f6669199b2d846b7f1ba
Jerry Ron Sunny <87520422+jerrygoodboi@users.noreply.github.com>
2023-05-08 16:53:14 +0530

    Update 39_perfect_num.c
    the for loop need only to run half times of that number

--- 39_perfect_num.c
+++ 39_perfect_num.c
@@ -9,7 +9,7 @@ int num, sum = 0;
 printf("Enter a number : ");
 scanf("%d", &num);
 
-for (int i = 1; i < num; i++) {
+for (int i = 1; i <= num/2; i++) {
     if (num % i == 0) {
         sum += i;
     }
