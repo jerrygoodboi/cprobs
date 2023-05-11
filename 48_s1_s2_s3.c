8099fa747cdcdfbdfc2576dbef767a7e00d2f52a
Jerry Ron Sunny <rabbitron295@gmail.com>
2023-05-11 12:49:58 +0530

    48_s1_s2_s3.c added

--- 48_s1_s2_s3.c
+++ 48_s1_s2_s3.c
@@ -1,18 +1,29 @@
 //35 s1,s2,s3 are 3 string *
 #include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
 #define pr printf
 #define sc scanf
 int main(){
-	char s1[100],s2[100];
+	char temp,s1[100],s2[100];
 	pr("Enter a string:");
-	fgets(s1,99,stdin);
+	gets(s1);
+	//scanf("%[^\n]-*%c",s1);
+	//fgets(s1,99,stdin);
 	pr("Enter next string:");
-	fgets(s2,99,stdin);
-	int st = strcmp(s1,s2);
-	if(st == 0){
+	gets(s2);
+	//scanf("%[^\n]-*%c",s2);
+	//fgets(s2,99,stdin);
+	
+	if(strcmp(s1,s2) == 0){
 		fprintf(stderr,"idk what to do with this qn but the given strings r == btw\n");
 	}
 	else{
-		fprintf(stderr,"idk what to do with this qn but the given strings r != btw\n");
+		char *s3 = (char *)malloc(sizeof(int)*(strlen(s1)+strlen(s2))+1);
+		strcat(s3,s1);
+		strcat(s3,s2);
+		puts(s3);
+		pr("Length of s3 is:%d\n",strlen(s3));
+		free(s3);
 	}
 }
